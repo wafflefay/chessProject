2350f311c5b6db9639cf07a28c9693035e2cb5d1
wafflefay <finny.waffle@gmail.com>
2025-01-17 17:25:36 -0800

    added something new

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("poasidghpaoidfhj");
+        System.out.println("poasidghpaoidfhj and something new");
     }
 }
