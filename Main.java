ae0d5ce5e0ad095fe342bc6698851022cc3ae133
wafflefay <finny.waffle@gmail.com>
2025-01-17 17:27:22 -0800

    added 123456789

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("poasidghpaoidfhj and something new");
+        System.out.println("123456789");
     }
 }
